f03f3182357cad41cf699e8683b4395acb068768
Pelado1998 <rafaelrodriguezsanz@gmail.com>
2020-10-25 20:54:43 -0300

    Falta justificar

--- src/Library/AllInOnePrinter.cs
+++ src/Library/AllInOnePrinter.cs
@@ -5,11 +5,11 @@
 //---------------------------------------------------------------------------------
 using System;
 using System.IO;
-
 namespace Full_GRASP_And_SOLID.Library
 {   
     public class AllInOnePrinter
     {
+        public AllInOnePrinter() {}
         public void PrintConsoleRecipe(Recipe recipe) => Console.WriteLine(recipe.GetTextToPrint());
         public void PrintFileRecipe(Recipe recipe) => File.WriteAllText("Recipe.txt", recipe.GetTextToPrint());
     }
